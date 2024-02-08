cd0c4d97f910b5c3492722e2999ca5e7b91eb147
George Perdrizet <george@perdrizet.org>
2024-02-08 13:12:30 -0500

    Updated generate and added helper script.

--- skylines/generate_from_checkpoint.py
+++ skylines/generate_from_checkpoint.py
@@ -1,22 +1,20 @@
 import sys
-from numpy.random import randn
 import tensorflow as tf
-import matplotlib.pyplot as plt
 import config
 import functions.training_functions as training_funcs
 
 if __name__ == '__main__':
-    #print("Num GPUs Available: ", len(tf.config.experimental.list_physical_devices('GPU')))
 
-    # collect comman line arguments
+    # collect command line arguments
     args = sys.argv[1:]
-    checkpoint = args[0]
+    checkpoint = int(args[0])
     num_images = int(args[1])
 
-    # Assign job to one specific GPU
-    with tf.device('/device:CPU:0'):
+    # Assign job first available GPU
+    with tf.device('/gpu:0'):
+
         # load model from checkpoint
-        model_checkpoint = f'{config.MODEL_CHECKPOINT_DIR}/generator_model_f{checkpoint}.h5'
+        model_checkpoint = f'{config.MODEL_CHECKPOINT_DIR}/generator_model_f{checkpoint:07d}'
         generator_model = tf.keras.models.load_model(model_checkpoint)
 
         # Check its architecture
