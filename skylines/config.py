cc686ce2bc17774c811b535de626402a83a7986e
George Perdrizet <george@perdrizet.org>
2024-02-08 08:17:49 -0500

    Updated repo structure and started working on the ability to resume training runs.

---
+++ skylines/config.py
@@ -0,0 +1,65 @@
+import os
+from datetime import datetime
+
+# Project name and run date
+PROJECT_NAME = 'skylines'
+CURRENT_DATE = datetime.today().strftime('%Y-%m-%d')
+
+########################################################################
+# Option to resume a training run ######################################
+########################################################################
+
+RESUME = True
+RESUME_RUN_DATE = '2024-02-07'
+
+########################################################################
+# Paths and directories ################################################
+########################################################################
+
+# Get path to this config file, we will use this
+# to define other paths to data files etc.
+path = os.path.dirname(os.path.realpath(__file__))
+
+# Use current date or resume data in file paths as needed
+
+if RESUME == True:
+    path_date = RESUME_RUN_DATE
+
+elif RESUME == False:
+    path_date = CURRENT_DATE
+
+IMAGE_DIR = f'{path}/image_datasets'
+RAW_IMAGE_DIR = f'{IMAGE_DIR}/raw_images'
+PROCESSED_IMAGE_DIR = f'{IMAGE_DIR}/training_images'
+TRAINING_IMAGE_DIR = PROCESSED_IMAGE_DIR
+MODEL_CHECKPOINT_DIR = f'{path}/training_checkpoints/{path_date}'
+SPECIMEN_DIR = f'{path}/specimens/{path_date}'
+IMAGE_OUTPUT_DIR = f'{path}/gan_output/{path_date}'
+
+
+########################################################################
+# Data related parameters ##############################################
+########################################################################
+
+MAX_CONCURRENCY = 2
+IMAGE_DIM = 1024
+SHUFFLE_BUFFER = 50
+
+########################################################################
+# DC-GANN parameters ###################################################
+########################################################################
+
+GPUS = [
+    '/job:localhost/replica:0/task:0/device:GPU:0',
+    '/job:localhost/replica:0/task:0/device:GPU:1',
+]
+
+GPU_PARALLELISM = 'central storage'
+LATENT_DIM = 100
+DISCRIMINATOR_LEARNING_RATE = 0.00005
+GENERATOR_LEARNING_RATE = 0.00005
+GAN_LEARNING_RATE = 0.00005
+BATCH_SIZE = int(2 * len(GPUS))
+EPOCHS = 100000
+
+CHECKPOINT_SAVE_FREQUENCY = 50
--- train.sh
+++ train.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
+# Convenience script to start DC-GANN training run. 
+# Sets some environment variables. See skylines/config.py for
+# additional run options
+
 # Increase tcmalloc report threshold to 36 GB
 export TCMALLOC_LARGE_ALLOC_REPORT_THRESHOLD=38654705664
-
-# Make sure display manager keeps its grubby mits off of the GPUs
-#sudo systemctl stop `cat /etc/X11/default-display-manager`
 
 # Set Tensorflow log level if desired:
 # 0 - (all logs shown)
@@ -20,9 +21,5 @@ export CUDA_VISIBLE_DEVICES=1,2
 # Prevent tensorflow from automapping all GPU memory
 export TF_FORCE_GPU_ALLOW_GROWTH=false
 
-# Clean up output directories
-# rm --force training_checkpoints/*
-# rm --force gan_output/*
-
 # Start GANN training run
-python dc-gan3.py
+python ./skylines/dc-gan3.py
